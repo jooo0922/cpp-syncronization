8bdfd63b96a8adafb3286a3e1298f905b6237474
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2024-10-09 22:54:38 +0900

    feat: mutex lock 기법으로 상호 배제 동기화 구현

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,22 @@
 
 #define NUM_THREAD 4
 
+// mutex 에서 자물쇠 역할을 하는 전역변수 lock 초기화
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 // 각 스레드가 접근하려는 공유 자원
 int shared = 0;
 
 void *foo(void *arg) {
   // 각 스레드가 수행할 작업을 정의한 함수
   // -> 공유 자원에 10000번 접근하여 +1 증가시킴
+  // 공유 자원에 접근하는 임계 구역 시작 위치
+  pthread_mutex_lock(&mutex); // 임계 구역을 잠그는 acquire 함수 역할
   for (int i = 0; i < 10000; i++) {
     shared += 1;
   }
+  pthread_mutex_unlock(&mutex); // 임계 구역 잠금을 해제하는 release 함수 역할
+  // 임계 구역 종료 위치
   return NULL;
 }
 
